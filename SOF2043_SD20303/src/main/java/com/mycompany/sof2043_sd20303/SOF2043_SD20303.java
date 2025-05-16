e28553937fc019431c4675c39e8db9968f252fd4
huyennk6 <huyennk6@fpt.edu.vn>
2025-05-16 17:01:42 +0700

    buoi 3

---
+++ SOF2043_SD20303/src/main/java/com/mycompany/sof2043_sd20303/SOF2043_SD20303.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sof2043_sd20303;
+
+/**
+ *
+ * @author Huyen
+ */
+public class SOF2043_SD20303 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
